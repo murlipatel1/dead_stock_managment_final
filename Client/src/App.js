c253b552f18d8b2888f68bdc0dc754b3072493f3
MURLI PATEL <100035961+murlipatel1@users.noreply.github.com>
2023-01-15 15:59:10 +0530

    Add files via upload

---
+++ Client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './Pages/Dashboard';
+import Histroy from './Pages/History';
+import Messages from './Pages/Messages';
+import Charts from './Pages/Charts';
+import ProductInfo from './Elements/productInfo';
+import DeadStock from './Pages/DeadStock';
+import HomePage from './Pages/HomePage';
+import AboutUs from './Pages/AboutUs';
+// import Blogs from '';
+
+
+function App() {
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path='/Dashboard' element={ <Dashboard key = "dashboard" /> } />
+      <Route path='/History' element = { <Histroy key="history" />} />
+      <Route path='/Messages' element = {<Messages key="message" />}/>
+      <Route path='/Charts' element = { <Charts key="charts" /> }/>
+      <Route path='/ProductInfo' element = {<ProductInfo key="ProductInfo" />} />
+      <Route path='/DeadStock' element = {<DeadStock />}/>
+      <Route path='/' element = { <HomePage /> }/>
+      <Route path='/about' element = {<AboutUs />}/>
+      {/* <Route path='/Blogs' element = {<Blogs />}/> */}
+     </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
